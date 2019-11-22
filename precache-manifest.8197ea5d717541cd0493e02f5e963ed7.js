d7ae1fcfce1d4e8d4284da4acfbd570cf994fd03
unknown <bassem.labbassi@gmail.com>
2019-11-22 18:52:18 +0100

    Updates

---
+++ precache-manifest.8197ea5d717541cd0493e02f5e963ed7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f8c4bd1bbed908e77dfd07955c8191a",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "a081eb4f94ce42fa543e",
+    "url": "/Weather-App/static/css/2.1d3c1dac.chunk.css"
+  },
+  {
+    "revision": "54a8e686468dfc579f65",
+    "url": "/Weather-App/static/css/main.539dfc6a.chunk.css"
+  },
+  {
+    "revision": "a081eb4f94ce42fa543e",
+    "url": "/Weather-App/static/js/2.9e9e7b79.chunk.js"
+  },
+  {
+    "revision": "54a8e686468dfc579f65",
+    "url": "/Weather-App/static/js/main.f54ee16d.chunk.js"
+  },
+  {
+    "revision": "140837169ee71281c2f5",
+    "url": "/Weather-App/static/js/runtime-main.44c725d4.js"
+  },
+  {
+    "revision": "8a72ebe3e43e0e544ae15ad0891d2cd7",
+    "url": "/Weather-App/static/media/pexels-photo-589840.8a72ebe3.jpeg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/Weather-App/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/Weather-App/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/Weather-App/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/Weather-App/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/Weather-App/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
